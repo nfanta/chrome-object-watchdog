cbe07bc4ee1fa8339d3c73f30bcc288cbbbc344b
Nicolas Fantagossi <nicolasfantagossi@gmail.com>
2017-12-25 20:31:00 -0300

    Adds newline at end of file.

--- devtools.js
+++ devtools.js
@@ -1,3 +1,3 @@
 chrome.devtools.panels.sources.createSidebarPane('Object watchdog', function(sidebar) {
-    sidebar.setPage('object-watchdog.html')
+    sidebar.setPage('dist/object-watchdog.html')
 })
